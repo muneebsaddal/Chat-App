54c0c8bd2fac786d4d06a2bfec4695a3bd8a750f
Muneeb Saddal <muneebsaddal@outlook.com>
2022-09-20 14:20:42 +0500

    logout option add welcome screen

--- public/src/components/Welcome.jsx
+++ public/src/components/Welcome.jsx
@@ -1,14 +1,20 @@
 import styled from "styled-components";
 import Robot from "../assets/robot.gif";
+import Logout from "./Logout";
 
 const Welcome = ({ currentUser }) => {
 	return (
 		<Container>
-			<img src={Robot} alt="Robot" />
-			<h1>
-				Welcome, <span>{currentUser.username}</span>
-			</h1>
-			<h3>Please select a Contact to start Chat</h3>
+			<div className="logout-container">
+				<Logout />
+			</div>
+			<div className="content">
+				<img src={Robot} alt="Robot" />
+				<h1>
+					Welcome, <span>{currentUser.username}</span>
+				</h1>
+				<h3>Please select a Contact to start Chat</h3>
+			</div>
 		</Container>
 	);
 };
@@ -16,21 +22,30 @@ const Welcome = ({ currentUser }) => {
 export default Welcome;
 
 const Container = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	width: 100%;
-	color: #ccc;
-	h1 {
-		font-size: 2rem;
-		font-weight: 500;
+	.logout-container {
+		display: flex;
+        justify-content: flex-end;
+        margin: 1rem 1rem 0 0;
 	}
-	h3 {
-        font-size: 1.4rem;
-		font-weight: 500;
-	}
-	img {
-		height: 20rem;
+	.content {
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+        height: 85%;
+		color: #ccc;
+		h1 {
+			font-size: 2rem;
+			font-weight: 500;
+		}
+		h3 {
+			font-size: 1.4rem;
+			font-weight: 500;
+		}
+		img {
+			height: 20rem;
+		}
 	}
 `;
